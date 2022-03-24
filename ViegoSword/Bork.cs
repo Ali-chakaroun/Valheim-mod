f72d6388828d1c560e5073c31ae9f1b6e9620e8a
Ali <83174544+Ali-chakaroun@users.noreply.github.com>
2022-03-24 15:25:41 +0200

    Add files via upload

---
+++ ViegoSword/Bork.cs
@@ -0,0 +1,162 @@
+﻿using BepInEx;
+using HarmonyLib;
+using JotunnLib.Entities;
+using JotunnLib.Managers;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+using Shared;
+using CheckDmg;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ViegoSword
+{
+  
+
+    [BepInPlugin("BladeoftheRuinedKing.Bork", "Blade of the Ruined King", "1.0.0")]
+    [BepInDependency(JotunnLib.JotunnLib.ModGuid)]
+    public  class Bork : BaseUnityPlugin
+    {
+        
+        private static List<string> resources = new List<string>();
+        private static List<string> amount = new List<string>();
+        static string[] items = new string[4];
+        static  int[] amounts = new int[4];
+        static int n;
+        static int a = 0;
+        static int m = 0;
+        private static void Fileread()
+        {
+           
+
+            List<string> lines = File.ReadAllLines(@"BepInEx\plugins\BorkRecipe.txt").ToList();
+            foreach (string line in lines)
+            {
+                if (!line.IsNullOrWhiteSpace())
+                {
+                    if (line.IndexOf("Resource:") != -1)
+                    {
+
+                        if (!line.Remove(0, 9).Trim().ToString().IsNullOrWhiteSpace())
+                        {
+                            resources.Add(line.Remove(0, 9).Trim());
+                        }
+                    }
+                    if (line.IndexOf("Amount:") != -1)
+                    {
+                        if (int.TryParse(line.Remove(0, 7).Trim(), out n) && !line.Remove(0, 7).Trim().ToString().IsNullOrWhiteSpace())
+                        {
+                            amount.Add(line.Remove(0, 7).Trim());
+                        }
+                    }
+                }
+            }
+            foreach (string res in resources)
+            {
+                items[a] = res;
+                //Debug.Log($"{items[a]}");
+                a += 1;
+
+            }
+            foreach (string am in amount)
+            {
+                amounts[m] = int.Parse(am);
+                //Debug.Log($"{amounts[m]}");
+                m +=  1;
+            }
+        }
+
+        private void Awake()
+        {
+            Fileread();
+            PrefabManager.Instance.PrefabRegister += RegisterPrefabs;
+            ObjectManager.Instance.ObjectRegister += InitObjects;
+        }
+
+        private void RegisterPrefabs(object sender, EventArgs e)
+        {
+            var Borkbundle = AssetBundleHelper.GetAssetBundleFromResources("bladeoftheruinedking");
+            var Bork = Borkbundle.LoadAsset<GameObject>("Assets/CustomItems/ViegoSword/Bladeoftheruinedking.prefab");
+            //var swordBlockItemDrop = swordBlock.GetComponent<ItemDrop>();
+
+            //PrefabManager.Instance.RegisterPrefab(swordBlock, "pipislongsword");
+            AccessTools.Method(typeof(PrefabManager), "RegisterPrefab", new Type[] { typeof(GameObject), typeof(string) }).Invoke(PrefabManager.Instance, new object[] { Bork, "viegosword" });
+
+        }
+        private void InitObjects(object sender, EventArgs e)
+        {
+            // Add block sword as an item
+            ObjectManager.Instance.RegisterItem("viegosword");
+
+            // Add magic armor as an item
+            //ObjectManager.Instance.RegisterItem("MagicArmor");
+
+            // Add a sample recipe for the example sword
+            ObjectManager.Instance.RegisterRecipe(new RecipeConfig()
+            {
+                // Name of the recipe (defaults to "Recipe_YourItem")
+                Name = "Recipe_viegosword",
+
+                // Name of the prefab for the crafted item
+                Item = "viegosword",
+
+                // Name of the prefab for the crafting station we wish to use
+                // Can set this to null or leave out if you want your recipe to be craftable in your inventory
+                CraftingStation = "forge",
+
+                RepairStation = "forge",
+                MinStationLevel = 3,
+
+                // List of requirements to craft your item
+                Requirements = new PieceRequirementConfig[]
+                
+                {
+
+                   new PieceRequirementConfig()
+                    {
+                        // Prefab name of requirement
+                        Item= items[0],
+
+                        // Amount required
+                        Amount = amounts[0]
+
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        // Prefab name of requirement
+                        Item= items[1],
+
+                        // Amount required
+                        Amount = amounts[1]
+
+                    },
+                      new PieceRequirementConfig()
+                    {
+                        // Prefab name of requirement
+                        Item= items[2],
+
+                        // Amount required
+                        Amount = amounts[2]
+
+                    },
+                        new PieceRequirementConfig()
+                    {
+                        // Prefab name of requirement
+                        Item= items[3],
+
+                        // Amount required
+                        Amount = amounts[3]
+
+                    }
+
+                }
+            });
+
+        }
+
+
+
+    }
+}
---
+++ ViegoSword/Damage.cs
@@ -0,0 +1,110 @@
+﻿using BepInEx;
+using HarmonyLib;
+using JotunnLib.Entities;
+using JotunnLib.Managers;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+using ViegoSword;
+namespace CheckDmg
+{
+    [BepInPlugin("checkdmg.ValheimMod", "Damge mod", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    class Damage : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("checkdmg.ValheimMod");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+        public static Character player;
+        public static float hp = 0;
+        public static bool Primaryattack = false;
+        public static bool Secondaryattack = false; 
+        public static bool Weaponhit = false;
+        public static bool Borkinuse = false;
+        public static bool attackqueue = false;
+        [HarmonyPatch(typeof(HitData), nameof(HitData.GetTotalDamage))]
+       public static class DMG 
+        {
+          
+            static void Postfix( float __result,HitData __instance)
+            {
+                player = __instance.GetAttacker();
+
+                if (Secondaryattack) {
+                    if ((player == Player.m_localPlayer && __result > 0) && Borkinuse)
+                    {
+                        
+                        //Debug.Log($"dealer is: {__instance.m_attacker.m_id}");
+                        //Debug.Log($"dmg is: {__result}");
+                        float dmg = __result;
+                        float percentage = 15;
+                        float total = (int)((percentage * dmg) / 100);
+                        total = Mathf.Min(total, 120);
+                        player.Heal((float)total);
+                        //Debug.Log($"healing for: {(float)total}");
+                        Secondaryattack = false;
+                        Primaryattack = false;
+                        Borkinuse = false;
+
+                    }
+                }
+
+              
+                
+            }
+        }
+
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.StartAttack))]
+        class healing5
+        {
+            static void Postfix(ref Humanoid __instance)
+            {
+                ItemDrop.ItemData currentWeapon = __instance.GetCurrentWeapon();
+                if (currentWeapon.m_shared.m_name == "Blade of the Ruined King" )
+                {
+                    Borkinuse = true;
+                  //  Debug.Log($"is bork in use {Borkinuse}");
+                }
+ 
+            }
+        }
+       
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.StartAttack))]
+        class secondarycheck
+        {
+            static void Postfix(ref bool ___m_secondaryAttack, ref bool ___m_attack,Humanoid __instance)
+            {
+                
+                    if ((__instance.InAttack() && !__instance.HaveQueuedChain()) || __instance.InDodge() || !__instance.CanMove() || __instance.IsKnockedBack() || __instance.IsStaggering() || __instance.InMinorAction())
+                    {
+                        attackqueue = true;
+                        //Debug.Log($"attacker {__instance.m_name} attackqueue: {attackqueue}");
+                    }
+                    else
+                    {
+                        attackqueue = false;
+                        //Debug.Log($"attacker {__instance.m_name} attackqueue: {attackqueue}");
+                    }
+                    if (___m_secondaryAttack && !attackqueue)
+                    {
+
+                        Secondaryattack = ___m_secondaryAttack;
+                        Primaryattack = false;
+                        //  Debug.Log($"using secondary: {___m_secondaryAttack}");
+                    }
+                    if (___m_attack && !attackqueue)
+                    {
+
+                        Primaryattack = ___m_attack;
+                        Secondaryattack = false;
+                        //  Debug.Log($"using Primary: {___m_attack}");
+                    }
+            }
+              
+        }
+    }
+}
